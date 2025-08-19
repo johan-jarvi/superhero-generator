0af7e193eef66bfd37ec568abb4bf11150a6758b
Johan Jarvi <johan.jarvi@nib.com.au>
2025-08-19 15:01:36 +1000

    Delete my examples

--- examples/batchGuesses.sh
+++
@@ -1,1 +0,0 @@
-node updateWeights.js --batch '[{"person":"Johan","guess":"Sam"},{"person":"Ali","guess":"Dom"},{"person":"Kemila","guess":"Ali"}]' Sam
--- examples/singleGuess.sh
+++
@@ -1,1 +0,0 @@
-node updateWeights.js Johan Sam Ali
